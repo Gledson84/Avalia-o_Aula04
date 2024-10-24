a00d32517f713fdf4842a528baf301144d51e3da
Gledson84 <gledsonpinheiro84@gmail.com>
2024-10-24 00:36:36 -0300

    Atualizar o Avaliação_aula04.py

--- Avaliação_aula04.py
+++ Avaliação_aula04.py
@@ -1,11 +1,12 @@
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
+n1 = int(input('Digite o primeiro número de um intervalo: '))
+n2 = int(input('Digite outro número para o fim do intervalo: '))
 soma = 0
-for i in range(n1, n2):
+for i in range(n1, n2 + 1):
     if i % 2 == 0:
         soma += i
-        print(soma)
 else:
-    print('Não há números pares no  intervalo')
-
+    if soma == 0:
+        print('Não há números pares no intervalo')
+    else:   
+        print(f' A soma dos números pares do intervalo é: {soma}.')
     
